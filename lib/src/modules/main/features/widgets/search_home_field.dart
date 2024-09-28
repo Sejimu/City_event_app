499e52c3d2d10bc669531dc141cd47d090a3d966
Sejimu <snursultan322@gmail.com>
2024-09-28 17:06:51 +0600

    search screen layer is done

--- lib/src/modules/main/features/widgets/search_home_field.dart
+++ lib/src/modules/main/features/widgets/search_home_field.dart
@@ -1,3 +1,6 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:clubcrafter/src/core/config/routes/app_routes.gr.dart';
+
 import '../../../../../generated/l10n.dart';
 import '../../../widgets/cruft_icon_button.dart';
 import '../../../../core/utils/extensions/extensions.dart';
@@ -9,35 +12,40 @@ const double _paddingUnit = 20;
 
 class SearchHomeField extends StatelessWidget {
   final TextEditingController controller;
+
   const SearchHomeField({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      cursorColor: AppColors.black,
-      controller: controller,
-      style: context.textTheme.bodySmall!.copyWith(color: AppColors.black),
-      maxLines: 1,
-      decoration: InputDecoration(
-          hintText: S.of(context).homeSearch,
-          hintStyle: context.textTheme.bodySmall!
-              .copyWith(color: AppColors.grey.withOpacity(0.3)),
-          prefixIcon: Padding(
-            padding: REdgeInsets.only(left: _paddingUnit, right: 10),
-            child: Icon(
-              Icons.search_outlined,
-              color: AppColors.grey.withOpacity(0.3),
-              size: 25.h,
+    return GestureDetector(
+      onTap: () => _goToSearchScreen(context),
+      child: TextFormField(
+        enabled: false,
+        cursorColor: AppColors.black,
+        controller: controller,
+        style: context.textTheme.bodySmall!.copyWith(color: AppColors.black),
+        maxLines: 1,
+        decoration: InputDecoration(
+            hintText: S.of(context).homeSearch,
+            hintStyle: context.textTheme.bodySmall!
+                .copyWith(color: AppColors.grey.withOpacity(0.3)),
+            prefixIcon: Padding(
+              padding: REdgeInsets.only(left: _paddingUnit, right: 10),
+              child: Icon(
+                Icons.search_outlined,
+                color: AppColors.grey.withOpacity(0.3),
+                size: 25.h,
+              ),
             ),
-          ),
-          suffixIcon: CruftIconButton(
-            onPressed: () {},
-            icon: Icons.sort_sharp,
-          ),
-          contentPadding:
-              REdgeInsets.symmetric(horizontal: _paddingUnit, vertical: 14),
-          enabledBorder: outlineborder(),
-          focusedBorder: outlineborder()),
+            suffixIcon: CruftIconButton(
+              onPressed: () {},
+              icon: Icons.sort_sharp,
+            ),
+            contentPadding:
+                REdgeInsets.symmetric(horizontal: _paddingUnit, vertical: 14),
+            enabledBorder: outlineborder(),
+            focusedBorder: outlineborder()),
+      ),
     );
   }
 
@@ -46,4 +54,7 @@ class SearchHomeField extends StatelessWidget {
         borderSide: BorderSide(color: AppColors.grey.withOpacity(0.4)),
         borderRadius: BorderRadius.circular(_paddingUnit));
   }
+
+  void _goToSearchScreen(BuildContext context) =>
+      context.pushRoute(const SearchRoute());
 }
